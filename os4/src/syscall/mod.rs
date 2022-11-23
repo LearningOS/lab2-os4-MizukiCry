983e4e2fcd7a9ec1895dcfa1d4efd1ccd00bebd3
MizukiCry <lanrtabe@qq.com>
2022-11-23 14:35:52 +0000

    update

--- os4/src/syscall/mod.rs
+++ os4/src/syscall/mod.rs
@@ -29,7 +29,6 @@ use crate::task::count_syscall;
 
 /// handle syscall exception with `syscall_id` and other arguments
 pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
-    // LAB1: You may need to update syscall info here.
     count_syscall(syscall_id);
     match syscall_id {
         SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
